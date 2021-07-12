8f2465442a37bc139755896494819db6f01d6ead
AhmedHome <ahmedmohamedaneng@gmail.com>
2021-07-12 23:05:46 +0200

    Bases Code Added

--- app/src/main/java/com/ahmed/moviesapp/ui/MainActivity.kt
+++ app/src/main/java/com/ahmed/moviesapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ahmed.moviesapp
+package com.ahmed.moviesapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ahmed.moviesapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
