0b548cd7318122e53637a987f734ab50f0c29399
AhmedHome <ahmedmohamedaneng@gmail.com>
2021-07-14 01:32:50 +0200

    edit repository

--- app/src/main/java/com/ahmed/moviesapp/data/Repository.kt
+++ app/src/main/java/com/ahmed/moviesapp/data/Repository.kt
@@ -4,16 +4,13 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.liveData
 import com.ahmed.moviesapp.api.MoviesApi
+import com.google.firebase.auth.FirebaseAuth
 import javax.inject.Inject
 
 class Repository
 @Inject constructor(
     private val api: MoviesApi
 ) {
-
-
-    suspend fun getMovies() = api.getMostPopularMovies(1)
-
 
     fun getMovies(page: Int) = Pager(
         config = PagingConfig(
